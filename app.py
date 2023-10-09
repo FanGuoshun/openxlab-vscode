c949a8457a2484cb1e64677711658ab4bf6f8915
fanguoshun <fanguoshun@pjlab.org.cn>
2023-10-09 16:31:04 +0800

    init

---
+++ app.py
@@ -0,0 +1,4 @@
+import os
+
+os.system("chmod +x start_server.sh")
+os.system('./start_server.sh')
---
+++ start_server.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "**** install openvscode-server runtime dependencies ****" &&
+  apt-get update &&
+  apt-get install -y \
+    jq \
+    libatomic1 \
+    nano \
+    net-tools \
+    netcat &&
+  echo "**** install openvscode-server ****" &&
+  if [ -z ${CODE_RELEASE+x} ]; then
+    CODE_RELEASE=$(curl -sX GET "https://api.github.com/repos/gitpod-io/openvscode-server/releases/latest" |
+      awk '/tag_name/{print $4;exit}' FS='[""]' |
+      sed 's|^openvscode-server-v||')
+  fi &&
+  mkdir -p /app/openvscode-server &&
+  curl -o \
+    /tmp/openvscode-server.tar.gz -L \
+    "https://github.com/gitpod-io/openvscode-server/releases/download/openvscode-server-v${CODE_RELEASE}/openvscode-server-v${CODE_RELEASE}-linux-x64.tar.gz" &&
+  tar xf \
+    /tmp/openvscode-server.tar.gz -C \
+    /app/openvscode-server/ --strip-components=1 &&
+  echo "**** clean up ****" &&
+  apt-get clean &&
+  rm -rf \
+    /tmp/* \
+    /var/lib/apt/lists/* \
+    /var/tmp/*
+
+echo "Starting VSCode Server..."
+
+exec /app/openvscode-server/bin/openvscode-server --host 0.0.0.0 --port 7860 --auth password
