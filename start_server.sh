02a62394580a015529c3cd578f5449f76e9ac634
fanguoshun <30681858+FanGuoshun@users.noreply.github.com>
2023-10-13 14:01:32 +0800

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-echo "**** install code-server ****" &&
-  mkdir -p ~/vscode/app/code-server &&
+  echo "**** install openvscode-server ****" &&
+  if [ -z ${CODE_RELEASE+x} ]; then
+    CODE_RELEASE=$(curl -sX GET "https://api.github.com/repos/gitpod-io/openvscode-server/releases/latest" |
+      awk '/tag_name/{print $4;exit}' FS='[""]' |
+      sed 's|^openvscode-server-v||')
+  fi &&
+  mkdir -p ~/vscode/app/openvscode-server &&
   mkdir -p ~/vscode/tmp &&
   curl -o \
-    ~/vscode/tmp/code-server.tar.gz -L \
-    "https://github.com/coder/code-server/releases/download/v4.17.1/code-server-4.17.1-linux-amd64.tar.gz" &&
+    ~/vscode/tmp/openvscode-server.tar.gz -L \
+    "https://github.com/gitpod-io/openvscode-server/releases/download/openvscode-server-v${CODE_RELEASE}/openvscode-server-v${CODE_RELEASE}-linux-x64.tar.gz" &&
   tar xf \
-    ~/vscode/tmp/code-server.tar.gz -C \
-    ~/vscode/app/code-server/ --strip-components=1 &&
+    ~/vscode/tmp/openvscode-server.tar.gz -C \
+    ~/vscode/app/openvscode-server/ --strip-components=1 &&
   echo "**** clean up ****" &&
   rm -rf \
     ~/vscode/tmp/*
 
 echo "Starting VSCode Server..."
 
-exec ~/vscode/app/code-server/bin/code-server --host 0.0.0.0 --port 7860
+exec ~/vscode/app/openvscode-server/bin/openvscode-server --host 0.0.0.0 --port 7860 --auth password --without-connection-token \"${@}\" --
